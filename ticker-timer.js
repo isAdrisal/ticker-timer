0d201b38a69c0678441245117393dffbec8fcbf1
isAdrisal <29012873+isAdrisal@users.noreply.github.com>
2021-05-11 14:28:14 +1000

    Begin consolidating functions inside constructor

--- ticker-timer.js
+++ ticker-timer.js
@@ -1,37 +1,3 @@
-const animationInterval = (ms, timerStart, signal, callback) => {
-  const frame = time => {
-    if (signal.aborted) return;
-    callback(time);
-    scheduleFrame(time);
-  };
-
-  const scheduleFrame = time => {
-    const elapsed = time - timerStart;
-    const roundedElapsed = Math.round(elapsed / ms) * ms;
-    const targetNext = timerStart + roundedElapsed + ms;
-    const delay = targetNext - performance.now();
-    setTimeout(() => requestAnimationFrame(frame), delay);
-  };
-
-  scheduleFrame(timerStart);
-};
-
-const padNum = number => String(number).padStart(2, '0');
-
-const formatTime = timeDiff => {
-  let day = padNum(Math.floor(timeDiff / (1000 * 60 * 60 * 24)));
-  let hour = padNum(Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
-  let minute = padNum(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60)));
-  let second = padNum(Math.floor((timeDiff % (1000 * 60)) / 1000));
-
-  return {
-    day,
-    hour,
-    minute,
-    second,
-  };
-};
-
 customElements.define(
   'ticker-timer',
   class TickerTimer extends HTMLElement {
@@ -90,24 +56,58 @@ customElements.define(
 
       shadowRoot.appendChild(style);
       shadowRoot.appendChild(container);
-    }
 
-    connectedCallback() {
       // Set up selectors
-      const segmentSelectors = {
+      this.segmentSelectors = {
         day: this.shadowRoot.querySelector('[data-day]'),
         hour: this.shadowRoot.querySelector('[data-hour]'),
         minute: this.shadowRoot.querySelector('[data-minute]'),
         second: this.shadowRoot.querySelector('[data-second]'),
       };
 
+      this.animationInterval = (ms, timerStart, signal, callback) => {
+        const frame = time => {
+          if (signal.aborted) return;
+          callback(time);
+          scheduleFrame(time);
+        };
+      
+        const scheduleFrame = time => {
+          const elapsed = time - timerStart;
+          const roundedElapsed = Math.round(elapsed / ms) * ms;
+          const targetNext = timerStart + roundedElapsed + ms;
+          const delay = targetNext - performance.now();
+          setTimeout(() => requestAnimationFrame(frame), delay);
+        };
+      
+        scheduleFrame(timerStart);
+      };
+      
+      this.padNum = number => String(number).padStart(2, '0');
+      
+      this.formatTime = timeDiff => {
+        let day = this.padNum(Math.floor(timeDiff / (1000 * 60 * 60 * 24)));
+        let hour = this.padNum(Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+        let minute = this.padNum(Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60)));
+        let second = this.padNum(Math.floor((timeDiff % (1000 * 60)) / 1000));
+      
+        return {
+          day,
+          hour,
+          minute,
+          second,
+        };
+      };
+    }
+
+    connectedCallback() {
       /**
        * Get settings from element attributes.
        * target — date-time string in the format: yyyy-mm-ddThh:mm:ss.mmm+hh:mm
        */
       const targetAttr = this.getAttribute('target');
       const target = targetAttr && targetAttr !== '' ? targetAttr : null;
-      const targetDateTime = target ? new Date(targetAttr).getTime() : null;
+      const targetDateTime = target ? Date.parse(targetAttr) : null;
 
       const now = targetDateTime ? Date.now() : null;
       const timerStart = Math.floor(
@@ -118,13 +118,13 @@ customElements.define(
 
       const printTime = (time, now, targetDateTime) => {
         let diffTime = targetDateTime ? targetDateTime - time + now : time;
-        const {day, hour, minute, second} = formatTime(diffTime);
+        const {day, hour, minute, second} = this.formatTime(diffTime);
         // TODO: update DOM with current time
         // TODO: use IntlLocale stuff to dynamically set labels
       };
 
       // Create an animation callback every second
-      animationInterval(1000, timerStart, controller.signal, time =>
+      this.animationInterval(1000, timerStart, controller.signal, time =>
         printTime(time)
       );
     }
